299d6f69d05f793909ad897b7b9a79bd0ea329a9
Nathan Mertins <nwmertins@gmail.com>
2020-02-01 08:54:11 -0600

    implement cell linking

--- spec/cell_spec.cr
+++ spec/cell_spec.cr
@@ -9,4 +9,23 @@ describe "Mazes::Cell" do
     cell.row.should eq(0)
     cell.column.should eq(0)
   end
+
+  it "can keep track of which neighboring cells are linked to this cell" do
+    cell = Mazes::Cell.new(0, 0)
+    neighbor = Mazes::Cell.new(1, 0)
+
+    cell.links.size.should eq(0)
+    neighbor.links.size.should eq(0)
+    cell.linked?(neighbor).should be_false
+    
+    cell.link(neighbor)
+    cell.links.size.should eq(1)
+    neighbor.links.size.should eq(1)
+    cell.linked?(neighbor).should be_true
+
+    cell.unlink(neighbor)
+    cell.links.size.should eq(0)
+    neighbor.links.size.should eq(0)
+    cell.linked?(neighbor).should be_false
+  end
 end
--- src/mazes/cell.cr
+++ src/mazes/cell.cr
@@ -2,10 +2,35 @@ module Mazes
   class Cell
     property row
     property column
-    
+    property north
+    property south
+    property east
+    property west
+
     def initialize(row : Int32, column : Int32)
       @row = row
       @column = column
+      @links = {} of Cell => Bool
+    end
+
+    def link(cell, bidirectional = true)
+      @links[cell] = true
+      cell.link(self, false) if bidirectional
+      self
+    end
+
+    def unlink(cell, bidirectional = true)
+      @links.delete(cell)
+      cell.unlink(self, false) if bidirectional
+      self
+    end
+
+    def links
+      @links.keys
+    end
+
+    def linked?(cell)
+      @links.has_key?(cell)
     end
   end
 end
