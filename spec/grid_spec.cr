6c509bde788083166fe41a911173eb8821db67c3
Nathan Mertins <nwmertins@gmail.com>
2020-02-03 09:00:32 -0600

    test neighbor size for every cell

--- spec/grid_spec.cr
+++ spec/grid_spec.cr
@@ -5,8 +5,25 @@ describe "Maze::Grid" do
     grid = Mazes::Grid.new(5, 5)
     grid.size.should eq(25)
 
-    grid[0, 0].as(Mazes::Cell).neighbors.size.should eq(2)  # corner
-    grid[1, 0].as(Mazes::Cell).neighbors.size.should eq(3)  # edge
-    grid[1, 1].as(Mazes::Cell).neighbors.size.should eq(4)  # interior
+    ### Corners
+    grid[0, 0].as(Mazes::Cell).neighbors.size.should eq(2)
+    grid[4, 0].as(Mazes::Cell).neighbors.size.should eq(2)
+    grid[0, 4].as(Mazes::Cell).neighbors.size.should eq(2)
+    grid[4, 4].as(Mazes::Cell).neighbors.size.should eq(2)
+
+    ### Edges
+    (1..3).each do |i|
+      grid[i, 0].as(Mazes::Cell).neighbors.size.should eq(3)
+      grid[i, 4].as(Mazes::Cell).neighbors.size.should eq(3)
+      grid[0, i].as(Mazes::Cell).neighbors.size.should eq(3)
+      grid[4, i].as(Mazes::Cell).neighbors.size.should eq(3)
+    end
+
+    ### Interior
+    (1..3).each do |x|
+      (1..3).each do |y|
+        grid[x, y].as(Mazes::Cell).neighbors.size.should eq(4)
+      end
+    end
   end
 end
