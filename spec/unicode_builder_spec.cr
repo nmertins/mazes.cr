40313d7955716837da379835bc612c5383673ed4
Nathan Mertins <nwmertins@gmail.com>
2020-02-29 08:45:01 -0600

    rewrite of to_unicode

--- spec/unicode_builder_spec.cr
+++ spec/unicode_builder_spec.cr
@@ -17,11 +17,11 @@ describe "Utils::UnicodeBuilder" do
     grid = Mazes::Grid.new(2, 2)
     output = Utils::UnicodeBuilder.to_unicode(grid)
     expected = String.build do |io|
-      io << "\u{250c 2500 2500 2500 2500 2500 2500 2500 2510}\n"
+      io << "\u{250c 2500 2500 2500 252c 2500 2500 2500 2510}\n"
       io << "\u2502   \u2502   \u2502\n"
       io << "\u{251c 2500 2500 2500 253c 2500 2500 2500 2524}\n"
       io << "\u2502   \u2502   \u2502\n"
-      io << "\u{2514 2500 2500 2500 2500 2500 2500 2500 2518}\n"
+      io << "\u{2514 2500 2500 2500 2534 2500 2500 2500 2518}\n"
     end
 
     output.should eq(expected)
--- src/utils/unicode_builder.cr
+++ src/utils/unicode_builder.cr
@@ -4,42 +4,20 @@ module Utils
   class UnicodeBuilder
     def self.to_unicode(grid : Mazes::Grid)
       String.build do |io|
-        io << "\u250c" + "\u{2500 2500 2500 2500}" * (grid.columns - 1) + "\u{2500 2500 2500 2510}" + "\n"
-
-        grid.each_row do |row|
-          is_last_row = row[0].row == (grid.rows - 1)
-          top = "\u2502"
-          bottom = is_last_row ? "\u2514" : "\u2502"
-
-          row.each do |cell|
-            is_last_cell = cell.column == (grid.columns - 1)
-            body = "   "
-            east_boundary = (cell.linked?(cell.east) ? " " : "\u2502")
-            top += body + east_boundary
-
-            south_boundary = (cell.linked?(cell.south) ? "   " : "\u{2500 2500 2500}")
-
-            unless is_last_row
-              if is_last_cell
-                corner =  "\u2502"
-              else
-                corner = "\u253c"
-              end
-            else
-              if is_last_cell
-                corner = "\u2518"
-              else
-                corner = "\u2500"
-              end
-            end
-
-            bottom += south_boundary + corner
-          end
-
-          io << top + "\n"
-          io << bottom + "\n"
-        end
+        io << build_top_row(grid)
       end
+    end
+
+    private def self.build_top_row(grid : Mazes::Grid)
+      ret = "\u250c"
+      (0...grid.columns-1).each do |x|
+        cell = grid[0, x].as(Mazes::Cell)
+        top_right_corner = cell.linked?(cell.east) ? "\u2500" : "\u252c"
+        ret += "\u{2500 2500 2500}" + top_right_corner
+      end
+      ret += "\u{2500 2500 2500 2510}\n"
+
+      ret
     end
   end
 end
