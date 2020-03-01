2e14fe79430b29c41760119b7ad5781c17fc4c99
Nathan Mertins <nwmertins@gmail.com>
2020-03-01 06:39:35 -0600

    realized I'm getting my coordinate system backwards

--- spec/unicode_builder_spec.cr
+++ spec/unicode_builder_spec.cr
@@ -29,16 +29,16 @@ describe "Utils::UnicodeBuilder" do
 
   it "should draw a U shape" do
     grid = Mazes::Grid.new(2, 2)
-    bottom_left = grid[0, 1].as(Mazes::Cell)
+    bottom_left = grid[1, 0].as(Mazes::Cell)
     bottom_left.link(bottom_left.north.as(Mazes::Cell)) if bottom_left.north
     bottom_left.link(bottom_left.east.as(Mazes::Cell)) if bottom_left.east
 
-    bottom_left.neighbors.size.should eq 2
+    bottom_left.links.size.should eq 2
 
     bottom_right = grid[1, 1].as(Mazes::Cell)
     bottom_right.link(bottom_right.north.as(Mazes::Cell)) if bottom_right.north
 
-    bottom_right.neighbors.size.should eq 2
+    bottom_right.links.size.should eq 2
 
     output = Utils::UnicodeBuilder.to_unicode(grid)
     expected = String.build do |io|
@@ -49,7 +49,6 @@ describe "Utils::UnicodeBuilder" do
       io << "\u{2514 2500 2500 2500 2500 2500 2500 2500 2518}\n"
     end
 
-    puts expected
     output.should eq expected
   end
 end
