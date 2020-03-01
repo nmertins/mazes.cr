9b808257a44eca8c06a8b84dc5c18e9549102f05
Nathan Mertins <nwmertins@gmail.com>
2020-03-01 06:47:40 -0600

    replace x,y with row,column
    Mixing the standard mathematical (x,y) coordinate system with the
    row, column format was causing me to get things backwards.

--- src/utils/unicode_builder.cr
+++ src/utils/unicode_builder.cr
@@ -5,9 +5,9 @@ module Utils
     def self.to_unicode(grid : Mazes::Grid)
       String.build do |io|
         io << build_top_row(grid)
-        (0...grid.rows).each do |y|
-          io << build_vertical_walls(y, grid)
-          io << build_horizontal_walls(y, grid) unless y == grid.rows-1
+        (0...grid.rows).each do |row|
+          io << build_vertical_walls(row, grid)
+          io << build_horizontal_walls(row, grid) unless row == grid.rows-1
         end
         io << build_bottom_row(grid)
       end
@@ -15,8 +15,8 @@ module Utils
 
     private def self.build_top_row(grid : Mazes::Grid)
       ret = "\u250c"
-      (0...grid.columns-1).each do |x|
-        cell = grid[x, 0].as(Mazes::Cell)
+      (0...grid.columns-1).each do |column|
+        cell = grid[column, 0].as(Mazes::Cell)
         top_right_corner = cell.linked?(cell.east) ? "\u2500" : "\u252c"
         ret += "\u{2500 2500 2500}" + top_right_corner
       end
@@ -27,8 +27,8 @@ module Utils
 
     private def self.build_vertical_walls(row : Int32, grid : Mazes::Grid)
       ret = "\u2502"
-      (0...grid.columns).each do |x|
-        cell = grid[x, row].as(Mazes::Cell)
+      (0...grid.columns).each do |column|
+        cell = grid[column, row].as(Mazes::Cell)
         right_border = cell.linked?(cell.east) ? " " : "\u2502"
         ret += "   " + right_border
       end
@@ -41,10 +41,10 @@ module Utils
       first_cell = grid[0, row].as(Mazes::Cell)
       ret = first_cell.linked?(first_cell.south) ? "\u2502" : "\u251c"
 
-      (0...grid.columns-1).each do |x|
-        cell = grid[x, row].as(Mazes::Cell)
+      (0...grid.columns-1).each do |column|
+        cell = grid[column, row].as(Mazes::Cell)
         horizontal_wall = cell.linked?(cell.south) ? "   " : "\u{2500 2500 2500}"
-        diag_neighbor = grid[x+1, row+1].as(Mazes::Cell)
+        diag_neighbor = grid[column+1, row+1].as(Mazes::Cell)
         north_wall_exists = !cell.linked?(cell.east)
         west_wall_exists = !cell.linked?(cell.south)
         east_wall_exists = !diag_neighbor.linked?(diag_neighbor.north)
@@ -92,8 +92,8 @@ module Utils
 
     private def self.build_bottom_row(grid : Mazes::Grid)
       ret = "\u2514"
-      (0...grid.columns-1).each do |x|
-        cell = grid[x, 0].as(Mazes::Cell)
+      (0...grid.columns-1).each do |column|
+        cell = grid[column, 0].as(Mazes::Cell)
         bottom_right_corner = cell.linked?(cell.east) ? "\u2500" : "\u2534"
         ret += "\u{2500 2500 2500}" + bottom_right_corner
       end
