011127d3b74557d5d4064aab28634d37121bebf6
Nathan Mertins <nwmertins@gmail.com>
2020-04-05 09:52:13 -0500

    class for tracking distance between cells

---
+++ spec/distances_spec.cr
@@ -0,0 +1,13 @@
+require "./spec_helper"
+
+describe "Mazes::Distances" do
+  it "should be initalized with a root cell and the distance to its root is 0" do
+    grid = Mazes::Grid.new(2, 2)
+    root = grid[0, 0].as(Mazes::Cell)
+
+    distances = Mazes::Distances.new(root)
+
+    distances[root].should eq 0
+  end
+
+end
