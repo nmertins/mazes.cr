c4ad3e76bf60c3dd78d26ee881a736449ac016e0
Nathan Mertins <nwmertins@gmail.com>
2020-04-11 08:27:56 -0500

    reworking method to satisfy type system

--- src/mazes/distance_grid.cr
+++ src/mazes/distance_grid.cr
@@ -1,16 +1,19 @@
-require "grid"
-require "distances"
+require "./grid"
+require "./distances"
 
 module Mazes
   class DistanceGrid < Grid
     property distances : Distances?
 
     def contents_of(cell)
-      if @distances && @distances[cell]?
-        @distances[cell].to_s(36)
-      else
-        super
+      unless @distances.nil?
+        distances = @distances.as(Distances)
+        if distances[cell]?
+          return distances[cell].to_s(36)
+        end
       end
+
+      super
     end
   end
 end
