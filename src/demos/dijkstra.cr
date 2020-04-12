6a719fa7ac9e42e5f8694c85b53082839da96ae4
Nathan Mertins <nwmertins@gmail.com>
2020-04-12 07:36:43 -0500

    use distances to find shortest path

--- src/demos/dijkstra.cr
+++ src/demos/dijkstra.cr
@@ -2,9 +2,16 @@ require "../mazes.cr"
 
 grid = Mazes::DistanceGrid.new(5, 5)
 Mazes::BinaryTree.on(grid)
+puts grid
 
 start = grid[0, 0].as(Mazes::Cell)
 distances = start.distances
 
-grid.distances = distances
+# Print results of Dijkstra's Algorithm
+# grid.distances = distances
+# puts grid
+
+puts "path from northeast corner to southwest corner:"
+goal = grid[grid.rows - 1, 0].as(Mazes::Cell)
+grid.distances = distances.path_to(goal)
 puts grid
