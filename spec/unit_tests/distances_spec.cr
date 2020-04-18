7cd2debac89034500d954089b7bf9e8ee18a768d
Nathan Mertins <nwmertins@gmail.com>
2020-04-18 08:15:08 -0500

    calculate shortest path and compare

--- spec/unit_tests/distances_spec.cr
+++ spec/unit_tests/distances_spec.cr
@@ -24,20 +24,27 @@ describe "Mazes::Distances" do
     # *   *---*   *   *   *
     # |   |       | 8 |   |
     # *---*---*---*   *   *
-    # | c   b   a   9 |   |
+    # | c   b   a   9 |   |   (a = 10, b = 11, c = 12)
     # *---*---*---*---*---*
+
+    knownShortestPath = [] of Mazes::Cell
 
     # row 1
     cell = grid[0, 0].as(Mazes::Cell)
+    knownShortestPath << cell
     cell.link(cell.east.as(Mazes::Cell)) if cell.east
     cell.link(cell.south.as(Mazes::Cell)) if cell.south
     cell = grid[0, 1].as(Mazes::Cell)
+    knownShortestPath << cell
     cell.link(cell.east.as(Mazes::Cell)) if cell.east
     cell = grid[0, 2].as(Mazes::Cell)
+    knownShortestPath << cell
     cell.link(cell.east.as(Mazes::Cell)) if cell.east
     cell = grid[0, 3].as(Mazes::Cell)
+    knownShortestPath << cell
     cell.link(cell.east.as(Mazes::Cell)) if cell.east
     cell = grid[0, 4].as(Mazes::Cell)
+    knownShortestPath << cell
     cell.link(cell.south.as(Mazes::Cell)) if cell.south
 
     # row 2
@@ -51,6 +58,7 @@ describe "Mazes::Distances" do
     cell = grid[1, 3].as(Mazes::Cell)
     cell.link(cell.east.as(Mazes::Cell)) if cell.east
     cell = grid[1, 4].as(Mazes::Cell)
+    knownShortestPath << cell
     cell.link(cell.south.as(Mazes::Cell)) if cell.south
 
     # row 3
@@ -61,9 +69,11 @@ describe "Mazes::Distances" do
     cell = grid[2, 2].as(Mazes::Cell)
     cell.link(cell.south.as(Mazes::Cell)) if cell.south
     cell = grid[2, 3].as(Mazes::Cell)
+    knownShortestPath << cell
     cell.link(cell.east.as(Mazes::Cell)) if cell.east
     cell.link(cell.south.as(Mazes::Cell)) if cell.south
     cell = grid[2, 4].as(Mazes::Cell)
+    knownShortestPath << cell
     cell.link(cell.south.as(Mazes::Cell)) if cell.south
 
     # row 4
@@ -72,20 +82,32 @@ describe "Mazes::Distances" do
     cell.link(cell.east.as(Mazes::Cell)) if cell.east
     cell = grid[3, 2].as(Mazes::Cell)
     cell = grid[3, 3].as(Mazes::Cell)
+    knownShortestPath << cell
     cell.link(cell.south.as(Mazes::Cell)) if cell.south
     cell = grid[3, 4].as(Mazes::Cell)
     cell.link(cell.south.as(Mazes::Cell)) if cell.south
 
     # row 5
     cell = grid[4, 0].as(Mazes::Cell)
+    knownShortestPath << cell
     cell.link(cell.east.as(Mazes::Cell)) if cell.east
     cell = grid[4, 1].as(Mazes::Cell)
+    knownShortestPath << cell
     cell.link(cell.east.as(Mazes::Cell)) if cell.east
     cell = grid[4, 2].as(Mazes::Cell)
+    knownShortestPath << cell
     cell.link(cell.east.as(Mazes::Cell)) if cell.east
     cell = grid[4, 3].as(Mazes::Cell)
+    knownShortestPath << cell
     cell = grid[4, 4].as(Mazes::Cell)
 
-    false.should be_true
+    # Calculate shortest path
+    root = grid[0, 0].as(Mazes::Cell)
+    goal = grid[4, 0].as(Mazes::Cell)
+    distances = root.distances
+    shortestPath = distances.path_to(goal)
+
+    shortestPath.cells.size.should eq 13
+    (knownShortestPath & shortestPath.cells).size.should eq 13
   end
 end
