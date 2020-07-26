c700c59f33c7f93b680dd88642004e841d901e9a
Nathan Mertins <nwmertins@gmail.com>
2020-07-26 07:28:57 -0500

    pull out into helper method

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,2 +1,22 @@
 require "spec"
 require "../src/mazes.cr"
+
+def create_2x2_U_grid
+  # Creates simple U-shaped grid:
+  #
+  # *---*---*
+  # |   |   |
+  # *   *   *
+  # |       |
+  # *---*---*
+
+  grid = Mazes::Grid.new(2,2)
+  top_left = grid[0, 0].as(Mazes::Cell)
+  top_left.link(top_left.south.as(Mazes::Cell)) if top_left.south
+
+  bottom_right = grid[1, 1].as(Mazes::Cell)
+  bottom_right.link(bottom_right.west.as(Mazes::Cell)) if bottom_right.west
+  bottom_right.link(bottom_right.north.as(Mazes::Cell)) if bottom_right.north
+
+  grid
+end
